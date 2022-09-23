166044338bb0cfbf9cc773fb3d84ac04e2aaf37d
tatusmatrix <Tatjana.Solvojova@rtu.lv>
2022-09-23 11:31:51 +0300

    Class_05

---
+++ linux_special_variables_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#https://www.tutorialspoint.com/unix/unix-special-variables.htm
+
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
+
+#echo "Please, enter some text: "
+#read var_string
+#echo "You have entered: $var_string"
