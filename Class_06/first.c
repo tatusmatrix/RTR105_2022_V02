f0ec738594d9479f001fd2fb2d521c479956ef5c
tatusmatrix <Tatjana.Solvojova@rtu.lv>
2022-09-30 11:32:23 +0300

    Class_06

---
+++ Class_06/first.c
@@ -0,0 +1,19 @@
+// komentāra viena rinda - // tiek apzīmēts tikai komentāra sākums
+// komentāra cita rinda
+
+/*
+Komentāra paragrāfs
+izmanto, piemēram, lai komentētu koda fragmentus
+*/
+
+int main()
+ {
+ //if()
+  //{
+  //
+  //}
+
+ return 0;
+
+ //return 100;
+ }
